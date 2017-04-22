451a68b7f9edecf7287329d31cb5b0c4ca76f588
jreider2 <julesreider@gmail.com>
2017-04-22 12:58:38 -0500

    4/22 Earth day! JavaDoc and a little junit tweeking

--- CargoListGeneratorTests/com/company/CargoListGeneratorTest.java
+++ CargoListGeneratorTests/com/company/CargoListGeneratorTest.java
@@ -7,23 +7,53 @@ import static org.junit.Assert.*;
 /**
  * Created by julianareider on 4/14/17.
  */
+
+
 public class CargoListGeneratorTest {
+    private CargoItem firstItem = new CargoItem(1, "firstItem", 1);
+    private CargoItem secondItem = new CargoItem(3, "secondItem", 4);
+    private CargoItem thirdItem = new CargoItem(4, "thirdItem", 5);
+    private CargoItem fourthItem = new CargoItem(5, "fourthItem", 7);
+    CargoItemList itemList = new CargoItemList();
+    CargoListGenerator testGenerator2 = new CargoListGenerator(itemList);
+    CargoListGenerator testGen = new CargoListGenerator();
+    CargoListGenerator testGen3 = new CargoListGenerator(itemList);
+
+    private CargoItem aItem = new CargoItem(10, "aItem", 60);
+    private CargoItem bItem = new CargoItem(20, "bItem", 100);
+    private CargoItem cItem = new CargoItem(30, "cItem", 120);
+    CargoItemList anotherItemList = new CargoItemList();
+    CargoListGenerator testGenerator3 = new CargoListGenerator(anotherItemList);
+
 
     @Test
-    public void fillValueMDArray() throws Exception {
-
-        CargoItem firstItem = new CargoItem(1, "firstItem", 1);
-        CargoItem secondItem = new CargoItem(3, "secondItem", 4);
-        CargoItem thirdItem = new CargoItem(4, "thirdItem", 5);
-        CargoItem fourthItem = new CargoItem(5, "fourthItem", 7);
+    public void calculateItemsLeftBehind() throws Exception {
         CargoItemList itemList = new CargoItemList();
+        itemList.addCargoItemToList(firstItem);
+        itemList.addCargoItemToList(secondItem);
+        itemList.addCargoItemToList(thirdItem);
+        itemList.addCargoItemToList(fourthItem);
+        CargoListGenerator testGen4 = new CargoListGenerator();
+        testGen4.setPotentialItems(itemList);
+        CargoItemList tempActualItems = new CargoItemList();
+        tempActualItems.addCargoItemToList(firstItem);
+        tempActualItems.addCargoItemToList(secondItem);
+        testGen4.setActualItems(tempActualItems);
+        testGen4.calculateItemsLeftBehind();
+        System.out.println("This is the End of my new tests. ");
+
+        //todo check that the all items from actual list is subtracted from potential list . and the remaining are stored in itemsLeftBehind
+
+
+    }
+    @Test
+    public void fillValueMDArray() throws Exception {
         int maxWeight = 7;
         itemList.addCargoItemToList(firstItem);
         itemList.addCargoItemToList(secondItem);
         itemList.addCargoItemToList(thirdItem);
         itemList.addCargoItemToList(fourthItem);
         int numItems = itemList.getSize();
-        CargoListGenerator testGenerator2 = new CargoListGenerator(itemList);
         testGenerator2.fillValueMDArray(maxWeight);
 
         String stringTestOfMDArray = "";
@@ -32,7 +62,6 @@ public class CargoListGeneratorTest {
             for (int incrementWeight = 0; incrementWeight <= maxWeight; incrementWeight++) {
                 stringTestOfMDArray += mdArray[indexOfAnItem][incrementWeight] + ",";
             }
-
             stringTestOfMDArray += "\n";
         }
         //todo add more tests
@@ -42,19 +71,14 @@ public class CargoListGeneratorTest {
     public void CargoListGenerator() throws Exception {
 
     }
-    //I wrote this test. It fails. This is a problem lol. It has to do with the generateList method as a whole and i think it fails because findSelectedItems isn't working right.
+
     @Test
     public void generateList() throws Exception {
-        CargoItem firstItem = new CargoItem(1, "firstItem", 1);
-        CargoItem secondItem = new CargoItem(3, "secondItem", 4);
-        CargoItem thirdItem = new CargoItem(4, "thirdItem", 5);
-        CargoItem fourthItem = new CargoItem(5, "fourthItem", 7);
-        CargoItemList itemList = new CargoItemList();
         itemList.addCargoItemToList(firstItem);
         itemList.addCargoItemToList(secondItem);
         itemList.addCargoItemToList(thirdItem);
         itemList.addCargoItemToList(fourthItem);
-        CargoListGenerator testGen = new CargoListGenerator();
+        //CargoListGenerator testGen = new CargoListGenerator();
         String expectedString = "Bring these items: \n" +
                 "\tItem: thirdItem, Weight: 4, Value: 5\n" +
                 "\tItem: secondItem, Weight: 3, Value: 4\n" +
@@ -62,14 +86,10 @@ public class CargoListGeneratorTest {
                 "Leave behind these items: \n"; //todo - once leave items behind calculation is done this should be updated
         assertEquals(expectedString, testGen.generateList(itemList, 7));
 
-        CargoItem aItem = new CargoItem(10, "aItem", 60);
-        CargoItem bItem = new CargoItem(20, "bItem", 100);
-        CargoItem cItem = new CargoItem(30, "cItem", 120);
-        CargoItemList anotherItemList = new CargoItemList();
         anotherItemList.addCargoItemToList(aItem);
         anotherItemList.addCargoItemToList(bItem);
         anotherItemList.addCargoItemToList(cItem);
-        CargoListGenerator testGenerator3 = new CargoListGenerator(anotherItemList);
+        //CargoListGenerator testGenerator3 = new CargoListGenerator(anotherItemList);
         expectedString = "Bring these items: \n" +
                 "\tItem: cItem, Weight: 30, Value: 120\n" +
                 "\tItem: bItem, Weight: 20, Value: 100\n" +
@@ -83,71 +103,37 @@ public class CargoListGeneratorTest {
 
     @Test
     public void calculateMaxValue() throws Exception {
-
-        /*CargoListGenerator testGenerator = new CargoListGenerator();
-
-        int[] weightArray = {1,3,4,5};
-        int[] valueArray = {1,4,5,7};
-
-        int returnValue0 = testGenerator.calculateMaxValue(7, weightArray, valueArray, 4);
-        System.out.println(returnValue0);
-        assertEquals(9, returnValue0);
-
-        int wt[] = new int[]{10, 20, 30};
-        int val[] = new int[]{60, 100, 120};
-        int  W = 50;
-        int n = val.length;
-        int returnValue1 = testGenerator.calculateMaxValue(W, wt, val, n);
-        System.out.println(returnValue1);
-        assertEquals(220, returnValue1);*/
-
-        CargoItem firstItem = new CargoItem(1, "firstItem", 1);
-        CargoItem secondItem = new CargoItem(3, "secondItem", 4);
-        CargoItem thirdItem = new CargoItem(4, "thirdItem", 5);
-        CargoItem fourthItem = new CargoItem(5, "fourthItem", 7);
-        CargoItemList itemList = new CargoItemList();
         itemList.addCargoItemToList(firstItem);
         itemList.addCargoItemToList(secondItem);
         itemList.addCargoItemToList(thirdItem);
         itemList.addCargoItemToList(fourthItem);
-        CargoListGenerator testGenerator2 = new CargoListGenerator(itemList);
+        //CargoListGenerator testGenerator2 = new CargoListGenerator(itemList);
         assertEquals(9, testGenerator2.calculateMaxValue(7)); //size would be four, second parameter
 
 
-        CargoItem aItem = new CargoItem(10, "aItem", 60);
-        CargoItem bItem = new CargoItem(20, "bItem", 100);
-        CargoItem cItem = new CargoItem(30, "cItem", 120);
-        CargoItemList anotherItemList = new CargoItemList();
         anotherItemList.addCargoItemToList(aItem);
         anotherItemList.addCargoItemToList(bItem);
         anotherItemList.addCargoItemToList(cItem);
-        CargoListGenerator testGenerator3 = new CargoListGenerator(anotherItemList);
-        //testGenerator3.fillValueMDArray(50);
+        //CargoListGenerator testGenerator3 = new CargoListGenerator(anotherItemList);
         assertEquals(220, testGenerator3.calculateMaxValue(50)); // size would be three second param
 
         CargoListGenerator testG = new CargoListGenerator(anotherItemList);
-       // testG.fillValueMDArray(1000);
         assertEquals(280, testG.calculateMaxValue(1000)); //tests for if all items can fit, if the value will be calculated correctly
-      //  testG.fillValueMDArray(0);
         assertEquals(0, testG.calculateMaxValue(0)); //test to see if the max weight is zero, if no value can be stored on the plane
 
     }
 
     @Test
     public void findSelectedItems() throws Exception {
-        CargoItem firstItem = new CargoItem(1, "firstItem", 1);
-        CargoItem secondItem = new CargoItem(3, "secondItem", 4);
-        CargoItem thirdItem = new CargoItem(4, "thirdItem", 5);
-        CargoItem fourthItem = new CargoItem(5, "fourthItem", 7);
-        CargoItemList itemList = new CargoItemList();
+
         itemList.addCargoItemToList(firstItem);
         itemList.addCargoItemToList(secondItem);
         itemList.addCargoItemToList(thirdItem);
         itemList.addCargoItemToList(fourthItem);
-        CargoListGenerator testGenerator3 = new CargoListGenerator(itemList);
-        testGenerator3.calculateMaxValue(7);
+        //CargoListGenerator testGen3 = new CargoListGenerator(itemList);
+        testGen3.calculateMaxValue(7);
         //findSelectedItems(5,8);
-        //assertEquals(9, testGenerator3.findSelectedItems(5, 8));
+        //assertEquals(9, testGen3.findSelectedItems(5, 8));
 
         //I think another additional way to test this method further is to
         // check that the sum of the values of all the selected items
